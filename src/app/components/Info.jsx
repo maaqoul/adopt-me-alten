820a9ae6dfa8d44457438e3ed449fa8b6ecc2472
hicham maaqoul <hicham.maaqoul@gmail.com>
2023-12-01 10:15:32 +0100

    portals

--- src/app/components/Info.jsx
+++ src/app/components/Info.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router";
 import fetchPet from "../../api/fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 const Info = () => {
   const { id } = useParams();
-
+  const [showModal, setShowModal] = useState(false);
   const results = useQuery(["details", id], fetchPet);
 
   if (results.isLoading) {
@@ -25,13 +27,25 @@ const Info = () => {
       <div>
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location} `}</h2>
-        <button>Adopt {name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {name}</button>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quam
           rerum nemo, porro ullam necessitatibus repellat unde quidem pariatur
           recusandae perferendis sed in corporis quis reiciendis. Officia
           obcaecati adipisci praesentium?
         </p>
+
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {name} ?</h1>
+              <div className="buttons">
+                <button>yes</button>
+                <button onClick={() => setShowModal(false)}>no</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
