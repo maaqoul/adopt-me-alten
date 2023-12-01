d99bf4714cb544424f274b1fb4dbfc5418bb13f0
hicham maaqoul <hicham.maaqoul@gmail.com>
2023-12-01 15:01:52 +0100

    Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import SearchParams from "./app/containers/SearchParams";
-import Info from "./app/components/Info";
 import AdoptedPetContext from "./context/AdoptPetContext";
 
 const queryClient = new QueryClient({
@@ -12,26 +10,45 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
+      suspense: true,
     },
   },
 });
 
+const Info = lazy(() => import("./app/components/Info"));
+const SearchParams = lazy(() => import("./app/containers/SearchParams"));
+
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <BrowserRouter>
-      <AdoptedPetContext.Provider value={adoptedPet}>
-        <QueryClientProvider client={queryClient}>
-          <header>
-            <Link to="/">Adopt me!</Link>
-          </header>
-          <Routes>
-            <Route path="/" element={<SearchParams />} />
-            <Route path="/info/:id" element={<Info />} />
-          </Routes>
-        </QueryClientProvider>
-      </AdoptedPetContext.Provider>
-    </BrowserRouter>
+    <div
+      className="p-0 m-0"
+      style={{
+        background: "url(http://pets-images.dev-apis.com/pets/wallpaperB.jpg)",
+      }}
+    >
+      <BrowserRouter>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <Suspense
+              fallback={
+                <div className="loading-pane">
+                  <h2 className="loader">⌛</h2>
+                </div>
+              }
+            >
+              <header>
+                <Link to="/">Adopt me!</Link>
+              </header>
+              <Routes>
+                <Route path="/" element={<SearchParams />} />
+                <Route path="/info/:id" element={<Info />} />
+              </Routes>
+            </Suspense>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
+      </BrowserRouter>
+    </div>
   );
 };
 
